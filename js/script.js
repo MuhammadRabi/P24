1e18a3e03fbb4737bc89de8a368d2402ecc2539b
Muhammad Rabi <vonrabi07@yahoo.com>
2021-10-02 14:04:43 +0200

    update

--- js/script.js
+++ js/script.js
@@ -7,3 +7,48 @@ menuToggle.addEventListener("click", () => {
   navBar.classList.toggle("active");
   menuToggle.classList.toggle("active");
 });
+
+// scroll to top
+
+let scrollToTop = document.querySelector(".scroll-to-top");
+
+window.addEventListener("scroll", scrollHandler);
+
+function scrollHandler() {
+  let windowHeight = window.pageYOffset;
+  windowHeight > 800
+    ? scrollToTop.classList.add("active")
+    : scrollToTop.classList.remove("active");
+}
+
+scrollToTop.addEventListener("click", ScrollTop);
+
+function ScrollTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+// scroll to section functionality
+
+let navLi = document.querySelectorAll("nav ul li a");
+
+function scrolltoSection(section) {
+  section.forEach((a) => {
+    a.addEventListener("click", (e) => {
+      e.preventDefault;
+      section.forEach((a) => {
+        a.classList.remove("active");
+      });
+      e.currentTarget.classList.add("active");
+      let clickedSec = document.querySelector(e.target.dataset.nav).offsetTop;
+      window.scrollTo({
+        top: clickedSec,
+        behavior: "smooth",
+      });
+    });
+  });
+}
+
+scrolltoSection(navLi);
